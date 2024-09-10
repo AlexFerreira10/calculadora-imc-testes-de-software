f46ee8fd064feead44da3ffcc39326058ce21118
AlexFerreira10 <correaferreiraalex92@email.com>
2024-09-09 21:04:56 -0300

    Adicionando um tratamento de erros.

--- src/main/java/com/uerj/calculadora/imc/CalculadoraImcApplication.java
+++ src/main/java/com/uerj/calculadora/imc/CalculadoraImcApplication.java
@@ -1,35 +1,53 @@
 package com.uerj.calculadora.imc;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-@SpringBootApplication
 public class CalculadoraImcApplication {
 
+	private static final double ALTURA_MAX = 2.2;
+	private static final double ALTURA_MIN = 0.5;
+	private static final double PESO_MAX = 300.0;
+	private static final double PESO_MIN = 2.5;
+
 	public static void main(String[] args) {
-		SpringApplication.run(CalculadoraImcApplication.class, args);
+
+
+		double peso = obterDouble("Digite o peso (kg): ", PESO_MIN, PESO_MAX);
+		double altura = obterDouble("Digite a altura (m): ", ALTURA_MIN, ALTURA_MAX);
+
+		String imc = CalculoIMC.resultado(peso, altura);
+		System.out.println("Seu IMC é: " + imc);
+
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
+	private static double obterDouble(String mensagem, double min, double max) {
+		Scanner sc = new Scanner(System.in);
 
-			Scanner sc = new Scanner(System.in);
+		double valor = 0.0;
+		boolean inputValido = false;
 
-			System.out.print("Digite o peso (kg): ");
-			double peso = sc.nextDouble();
-
-			System.out.print("Digite a altura (m): ");
-			double altura = sc.nextDouble();
-
-			String imc = CalculoIMC.resultado(peso, altura);
-			System.out.println("Seu IMC é: " + imc);
-
-			sc.close();
-		};
+		while (!inputValido) {
+			System.out.print(mensagem);
+			try {
+				if (sc.hasNextDouble()) {
+					valor = sc.nextDouble();
+					if (valor >= min && valor <= max) {
+						inputValido = true;
+					} else {
+						System.out.println("Valor fora do intervalo. Digite um número entre " + min + " e " + max + ".");
+					}
+				} else {
+					System.out.println("Entrada inválida. Digite um número válido.");
+					sc.next();
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Erro de entrada: " + e.getMessage());
+				sc.next();
+			} finally {
+				sc.close();
+			}
+		}
+		return valor;
 	}
 }
